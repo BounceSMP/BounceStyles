535cffedc67e96ac1a7d0b8fa2f67caa1e5542cc
Ryan <850178+RyanV97@users.noreply.github.com>
2022-11-23 19:52:51 +0000

    Updated to GeckoLib 3.0.77 Made a Mixin fix for Armor rendering in Vanity Slots

---
+++ src/main/java/dev/bsmp/bouncestyles/mixin/ArmorFeatureRendererMixin.java
@@ -0,0 +1,29 @@
+package dev.bsmp.bouncestyles.mixin;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.feature.ArmorFeatureRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ItemStack;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import dev.bsmp.bouncestyles.item.StyleItem;
+
+@Mixin(ArmorFeatureRenderer.class)
+public abstract class ArmorFeatureRendererMixin<T extends LivingEntity, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>> {
+
+    @Inject(method = "renderArmor", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/feature/ArmorFeatureRenderer;renderArmorParts(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/item/ArmorItem;ZLnet/minecraft/client/render/entity/model/BipedEntityModel;ZFFFLjava/lang/String;)V", ordinal = 2), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private void styleItemCheck(MatrixStack matrices, VertexConsumerProvider vertexConsumers, T entity, EquipmentSlot armorSlot, int light, A model, CallbackInfo ci, ItemStack itemStack, ArmorItem armorItem, boolean bl, boolean bl2) {
+        if(armorItem instanceof StyleItem)
+            ci.cancel();
+    }
+
+}
--- src/main/java/dev/bsmp/bouncestyles/mixin/PlayerRendererMixin.java
+++ src/main/java/dev/bsmp/bouncestyles/mixin/PlayerRendererMixin.java
@@ -23,39 +23,41 @@ import dev.emi.trinkets.api.TrinketsApi;
 public class PlayerRendererMixin {
 
     @Inject(method = "setModelPose", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/AbstractClientPlayerEntity;isInSneakingPose()Z", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void checkStyleVisibility(AbstractClientPlayerEntity player, CallbackInfo ci, PlayerEntityModel playerEntityModel) {
+    private void checkStyleVisibility(AbstractClientPlayerEntity player, CallbackInfo ci, PlayerEntityModel<?> playerEntityModel) {
         Optional<TrinketComponent> o = TrinketsApi.getTrinketComponent(player);
-        if(o.isPresent()) {
-            TrinketComponent component = o.get();
-            for(Pair<SlotReference, ItemStack> pair : component.getEquipped(stack -> stack.getItem() instanceof StyleItem)) {
-                StyleItem item = (StyleItem) pair.getRight().getItem();
-                if(item.hiddenParts != null && !item.hiddenParts.isEmpty()) {
-                    for(String s : item.hiddenParts) {
-                        switch (s) {
-                            case "head":
-                                playerEntityModel.head.visible = false;
-                                playerEntityModel.hat.visible = false;
-                                break;
-                            case "body":
-                                playerEntityModel.body.visible = false;
-                                break;
-                            case "left_arm":
-                                playerEntityModel.leftArm.visible = false;
-                                playerEntityModel.leftSleeve.visible = false;
-                                break;
-                            case "right_arm":
-                                playerEntityModel.rightArm.visible = false;
-                                playerEntityModel.rightSleeve.visible = false;
-                                break;
-                            case "left_leg":
-                                playerEntityModel.leftLeg.visible = false;
-                                playerEntityModel.leftPants.visible = false;
-                                break;
-                            case "right_leg":
-                                playerEntityModel.rightLeg.visible = false;
-                                playerEntityModel.rightPants.visible = false;
-                                break;
-                        }
+        if(o.isPresent())
+            for(Pair<SlotReference, ItemStack> pair : o.get().getEquipped(stack -> stack.getItem() instanceof StyleItem))
+                hideParts(playerEntityModel, (StyleItem) pair.getRight().getItem());
+
+        for(ItemStack itemStack : player.getArmorItems())
+            if(itemStack.getItem() instanceof StyleItem)
+                hideParts(playerEntityModel, (StyleItem) itemStack.getItem());
+    }
+
+    private void hideParts(PlayerEntityModel<?> playerEntityModel, StyleItem item) {
+        if(item.hiddenParts != null && !item.hiddenParts.isEmpty()) {
+            for(String s : item.hiddenParts) {
+                switch (s) {
+                    case "head" -> {
+                        playerEntityModel.head.visible = false;
+                        playerEntityModel.hat.visible = false;
+                    }
+                    case "body" -> playerEntityModel.body.visible = false;
+                    case "left_arm" -> {
+                        playerEntityModel.leftArm.visible = false;
+                        playerEntityModel.leftSleeve.visible = false;
+                    }
+                    case "right_arm" -> {
+                        playerEntityModel.rightArm.visible = false;
+                        playerEntityModel.rightSleeve.visible = false;
+                    }
+                    case "left_leg" -> {
+                        playerEntityModel.leftLeg.visible = false;
+                        playerEntityModel.leftPants.visible = false;
+                    }
+                    case "right_leg" -> {
+                        playerEntityModel.rightLeg.visible = false;
+                        playerEntityModel.rightPants.visible = false;
                     }
                 }
             }
