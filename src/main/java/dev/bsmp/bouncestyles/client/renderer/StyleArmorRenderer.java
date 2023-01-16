141b4492857d4679290a635063613426b4f68840
ryanv <850178+RyanV97@users.noreply.github.com>
2023-01-16 14:55:04 +0000

    Hidden Parts & Fixed a fucky-wucky

--- src/main/java/dev/bsmp/bouncestyles/client/renderer/StyleArmorRenderer.java
+++ src/main/java/dev/bsmp/bouncestyles/client/renderer/StyleArmorRenderer.java
@@ -5,11 +5,13 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import dev.bsmp.bouncestyles.item.StyleItem;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.ForgeHooksClient;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
@@ -26,10 +28,12 @@ public class StyleArmorRenderer extends GeoArmorRenderer<StyleItem> implements I
     @Override
     public <T extends LivingEntity, M extends EntityModel<T>> void render(ItemStack stack, SlotContext slotContext, PoseStack matrixStack, RenderLayerParent<T, M> renderLayerParent, MultiBufferSource renderTypeBuffer, int light, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         EquipmentSlot slot = getEquipmentSlotForTrinketSlot(slotContext);
-        if(slot != null && stack != null && stack.getItem() instanceof StyleItem item) {
-            LivingEntity entity = slotContext.entity();
+        LivingEntity entity = slotContext.entity();
 
-            renderLayerParent.getModel().copyPropertiesTo(this);
+        if(slot != null && entity instanceof Player) {
+            StyleItem item = (StyleItem) stack.getItem();
+
+            ((PlayerModel<?>) renderLayerParent.getModel()).copyPropertiesTo(this);
             setCurrentItem(entity, stack, slot);
             applySlot(slot);
 
@@ -46,6 +50,39 @@ public class StyleArmorRenderer extends GeoArmorRenderer<StyleItem> implements I
             case StyleItem.FeetStyleItem.curioSlot -> EquipmentSlot.FEET;
             default -> null;
         };
+    }
+
+    public static void hideParts(PlayerModel<?> playerEntityModel, StyleItem item) {
+        if(item.hiddenParts != null && !item.hiddenParts.isEmpty()) {
+            for(String s : item.hiddenParts) {
+                switch (s) {
+                    case "head" -> {
+                        playerEntityModel.head.visible = false;
+                        playerEntityModel.hat.visible = false;
+                    }
+                    case "body" -> {
+                        playerEntityModel.body.visible = false;
+                        playerEntityModel.jacket.visible = false;
+                    }
+                    case "left_arm" -> {
+                        playerEntityModel.leftArm.visible = false;
+                        playerEntityModel.leftSleeve.visible = false;
+                    }
+                    case "right_arm" -> {
+                        playerEntityModel.rightArm.visible = false;
+                        playerEntityModel.rightSleeve.visible = false;
+                    }
+                    case "left_leg" -> {
+                        playerEntityModel.leftLeg.visible = false;
+                        playerEntityModel.leftPants.visible = false;
+                    }
+                    case "right_leg" -> {
+                        playerEntityModel.rightLeg.visible = false;
+                        playerEntityModel.rightPants.visible = false;
+                    }
+                }
+            }
+        }
     }
 
 }
--- src/main/java/dev/bsmp/bouncestyles/mixin/PlayerRendererMixin.java
+++ src/main/java/dev/bsmp/bouncestyles/mixin/PlayerRendererMixin.java
@@ -1,5 +1,7 @@
 package dev.bsmp.bouncestyles.mixin;
 
+import dev.bsmp.bouncestyles.client.renderer.StyleArmorRenderer;
+import net.minecraft.world.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,53 +13,28 @@ import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.world.item.ItemStack;
 import dev.bsmp.bouncestyles.item.StyleItem;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
+
+import java.util.Map;
 
 @Mixin(PlayerRenderer.class)
 public class PlayerRendererMixin {
 
     @Inject(method = "setModelProperties", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/AbstractClientPlayer;isCrouching()Z", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
     private void checkStyleVisibility(AbstractClientPlayer player, CallbackInfo ci, PlayerModel<?> playerEntityModel) {
-//        Optional<TrinketComponent> o = TrinketsApi.getTrinketComponent(player);
-//        if(o.isPresent())
-//            for(Tuple<SlotReference, ItemStack> pair : o.get().getEquipped(stack -> stack.getItem() instanceof StyleItem))
-//                hideParts(playerEntityModel, (StyleItem) pair.getB().getItem());
+        CuriosApi.getCuriosHelper().getEquippedCurios(player).ifPresent(handler -> {
+            for(int i = 0; i < handler.getSlots(); i++) {
+                Item item = handler.getStackInSlot(i).getItem();
+                if(item instanceof StyleItem) {
+                    StyleArmorRenderer.hideParts(playerEntityModel, (StyleItem) item);
+                }
+            }
+        });
 
         for(ItemStack itemStack : player.getArmorSlots())
             if(itemStack.getItem() instanceof StyleItem)
-                hideParts(playerEntityModel, (StyleItem) itemStack.getItem());
-    }
-
-    private void hideParts(PlayerModel<?> playerEntityModel, StyleItem item) {
-        if(item.hiddenParts != null && !item.hiddenParts.isEmpty()) {
-            for(String s : item.hiddenParts) {
-                switch (s) {
-                    case "head" -> {
-                        playerEntityModel.head.visible = false;
-                        playerEntityModel.hat.visible = false;
-                    }
-                    case "body" -> {
-                        playerEntityModel.body.visible = false;
-                        playerEntityModel.jacket.visible = false;
-                    }
-                    case "left_arm" -> {
-                        playerEntityModel.leftArm.visible = false;
-                        playerEntityModel.leftSleeve.visible = false;
-                    }
-                    case "right_arm" -> {
-                        playerEntityModel.rightArm.visible = false;
-                        playerEntityModel.rightSleeve.visible = false;
-                    }
-                    case "left_leg" -> {
-                        playerEntityModel.leftLeg.visible = false;
-                        playerEntityModel.leftPants.visible = false;
-                    }
-                    case "right_leg" -> {
-                        playerEntityModel.rightLeg.visible = false;
-                        playerEntityModel.rightPants.visible = false;
-                    }
-                }
-            }
-        }
+                StyleArmorRenderer.hideParts(playerEntityModel, (StyleItem) itemStack.getItem());
     }
 
 }
